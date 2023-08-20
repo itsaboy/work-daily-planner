ca611f5e3abdd38f524ba41e62a7a9a5aa09dfa7
Zach <itsaboy3366@gmail.com>
2023-08-19 22:44:45 -0400

    centered header and notification text, changed the starting point of hours counted in HTML and JS, swapped arrow functions for regular functions in two places that caused critical bugs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,45 +3,43 @@ $().ready(() => {
   $("#currentDay").text(dayjs().format("dddd MMMM D, YYYY"));
 
   const time = () => {
-    let activeHour = dayjs().hour();
+    let activeHour = dayjs().subtract(8, "hours").hour();
 
-    $(".time-block").each(() => {
-      let blockID = $(".time-block").attr("id").split("-")[1];
+    $(".time-block").each(function () {
+      let blockID = parseInt($(this).attr("id").split("-")[1]);
 
       if (blockID < activeHour) {
-        $(".time-block").addClass("past");
+        $(this).addClass("past");
       } else if (blockID === activeHour) {
-        $(".time-block").removeClass("past");
-        $(".time-block").addClass("present");
-      } else {
-        $(".time-block").removeClass("past");
-        $(".time-block").removeClass("present");
-        $(".time-block").addClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else if (blockID > activeHour) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
       };
     });
 
-    console.log(activeHour);
-
+    console.log(activeHour); // remove later
   };
 
   // Loads data from local storage
   const loadData = () => {
-    $("hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   };
 
   // Saves data to local storage on save button click
   const saveData = () => {
-    $(".saveBtn").on("click", () => {
-      let hour = $(".saveBtn").parent().attr("id");
-      let item = $(".saveBtn").siblings(".description").val();
+    $(".saveBtn").on("click", function () {
+      let hour = $(this).parent().attr("id");
+      let item = $(this).siblings(".description").val();
       
       localStorage.setItem(hour, item);
 
@@ -63,5 +61,4 @@ $().ready(() => {
   loadData();
   saveData();
 
-  
 });
